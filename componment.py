b634da262c1396630ac00ae4686ee2ed523718da
lugf <luusuu@126.com>
2017-11-04 09:41:21 +0800

    add files

---
+++ componment.py
@@ -0,0 +1,147 @@
+import base64
+import urlparse
+import json
+from tlslite.utils import keyfactory
+import oauth2 as oauth
+
+
+class SignatureMethod_RSA_SHA1(oauth.SignatureMethod):
+    name = 'RSA-SHA1'
+
+    def signing_base(self, request, consumer, token):
+        if not hasattr(request, 'normalized_url') or request.normalized_url is None:
+            raise ValueError("Base URL for request is not set.")
+
+        sig = (
+            oauth.escape(request.method),
+            oauth.escape(request.normalized_url),
+            oauth.escape(request.get_normalized_parameters()),
+        )
+
+        key = '%s&' % oauth.escape(consumer.secret)
+        if token:
+            key += oauth.escape(token.secret)
+        raw = '&'.join(sig)
+        return key, raw
+
+    def sign(self, request, consumer, token):
+        """Builds the base signature string."""
+        key, raw = self.signing_base(request, consumer, token)
+
+        with open('../mykey.pem', 'r') as f:
+            data = f.read()
+        privateKeyString = data.strip()
+
+        privatekey = keyfactory.parsePrivateKey(privateKeyString)
+        signature = privatekey.hashAndSign(raw)
+
+        return base64.b64encode(signature)
+
+consumer_key = 'OauthKey'
+consumer_secret = 'dont_care'
+consumer = oauth.Consumer(consumer_key, consumer_secret)
+data_url = 'http://bug.chenyee.com:8080/rest/api/2/issue/SWW1616A-138'
+mother_project_url = 'http://bug.chenyee.com:8080/rest/api/2/project/SW17W04A'
+
+access_token = {'oauth_token': 'QHnpXBfHjXhYKfTMVDLMTyInQCefANgv', 'oauth_token_secret': 'k20W95a4ncIhSwBAcvlrXsIQn1IlZzcK'}
+accessToken = oauth.Token(access_token['oauth_token'], access_token[
+                          'oauth_token_secret'])
+client = oauth.Client(consumer, accessToken)
+client.set_signature_method(SignatureMethod_RSA_SHA1())
+
+resp, content = client.request(mother_project_url, "GET")
+resp, content = client.request(mother_project_url, "GET")
+mother_project_url = 'http://bug.chenyee.com:8080/rest/api/2/project'
+resp, content = client.request(mother_project_url, "GET")
+headers = {"Content-Type": "application/json"}
+resp, content = client.request(method="POST", url=mother_project_url, headers=headers, body=project_info)
+resp, content = client.request(method="POST", uri=mother_project_url, headers=headers, body=j)
+update_project_info = {"notificationScheme": 10000}
+up = json.dumps(update_project_info)
+update_project_url = 'http://bug.chenyee.com:8080/rest/api/2/project/TEST'
+resp, content = client.request(method="PUT", uri=update_project_url, headers=headers, body=j)
+resp, content = client.request(method="PUT", uri=update_project_url, headers=headers, body=up)
+update_project_info = {"workflowScheme": 10200}
+up = json.dumps(update_project_info)
+resp, content = client.request(method="PUT", uri=update_project_url, headers=headers, body=up)
+component_url = 'http://bug.chenyee.com:8080/rest/api/2/component/'
+mother_project_url
+resp, projects = client.request(mother_project_url, "GET")
+projects_dir = json.loads(projects)
+projects_dir
+for project in projects_dir:
+    print project['key']
+project_keys = []
+for project in projects_dir:
+    print project['key']
+    project_keys.append(project['key'])
+project_keys
+project_keys.remove('TEMP')
+project_keys.remove('TEST')
+project_keys
+history
+mother_project_url
+com_project_url = mother_project_url + project_keys[0] + '/components'
+com_project_url
+com_project_url = mother_project_url + '/' + project_keys[0] + '/components'
+com_project_url
+resp, com = client.request(com_project_url, "GET")
+com_dir = json.loads(com)
+com_dir
+history
+com_dir[0]
+com_dir[0]("id")
+com_dir[0]["id"]
+com_dir[0]["id", "name"]
+com_dir[0][("id", "name")]
+com_dir[0][["id", "name"]]
+com_dir[0].keys()
+com_dirs = []
+history
+for project in project_keys:
+    com_project_url = mother_project_url + '/' + project + '/components'
+    resp, com = client.request(com_project_url, "GET")
+    com_dirs.append(json.loads(com))
+len(com_dirs)
+len(project_keys)
+com_dirs[0][0]
+history
+jira_server_url = "http://bug.chenyee.com:8080"
+update_com_url = jira_server_url + "/rest/api/2/component/"
+update_com_content = json.dumps({'name': u'M-MISC(驱动)'})
+update_com_content
+update_com_url1 = update_com_url + com_dirs[0][0]
+update_com_url1 = update_com_url + com_dirs[0][0]('id')
+update_com_url1 = update_com_url + com_dirs[0][0]['id']
+update_com_url1
+update_com_content
+resp, content = client.request(method="PUT", uri=update_com_url1, headers=headers, body=update_com_content)
+resp
+content
+com_dirs[0]
+com_dirs[1]
+history
+com_dirs[0][0]
+need_update_bsp_id = []
+need_update_sw_id = []
+for coms_project in com_dirs:
+    for com in coms_project:
+        if com['name'] == u'M-MISC':
+            need_update_sw_id.append(com['id'])
+        elif com['name'] == u'A-AAMISC':
+            need_update_bsp_id.append(com['id'])
+need_update_sw_id
+need_update_bsp_id
+len(need_update_bsp_id)
+len(need_update_sw_id)
+need_update_bsp_id[0]
+need_update_sw_id[0]
+for bsp_id in need_update_bsp_id:
+    update_com_url = jira_server_url + "/rest/api/2/component/" + bsp_id
+    resp, content = client.request(method="PUT", uri=update_com_url, headers=headers, body=update_com_content)
+    print resp['status']
+update_com_content = json.dumps({'name': u'M-MISC(软件)'})
+for sw_id in need_update_sw_id:
+    update_com_url = jira_server_url + "/rest/api/2/component/" + sw_id
+    resp, content = client.request(method="PUT", uri=update_com_url, headers=headers, body=update_com_content)
+    print resp['status']
---
+++ rest_jira.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import base64
+import urlparse
+import json
+from tlslite.utils import keyfactory
+import oauth2 as oauth
+
+
+jira_server_url = "http://bug.chenyee.com:8080"
+headers = {"Content-Type": "application/json"}
+
+
+class SignatureMethod_RSA_SHA1(oauth.SignatureMethod):
+    name = 'RSA-SHA1'
+
+    def signing_base(self, request, consumer, token):
+        if not hasattr(request, 'normalized_url') or request.normalized_url is None:
+            raise ValueError("Base URL for request is not set.")
+
+        sig = (
+            oauth.escape(request.method),
+            oauth.escape(request.normalized_url),
+            oauth.escape(request.get_normalized_parameters()),
+        )
+
+        key = '%s&' % oauth.escape(consumer.secret)
+        if token:
+            key += oauth.escape(token.secret)
+        raw = '&'.join(sig)
+        return key, raw
+
+    def sign(self, request, consumer, token):
+        """Builds the base signature string."""
+        key, raw = self.signing_base(request, consumer, token)
+
+        with open('../mykey.pem', 'r') as f:
+            data = f.read()
+        privateKeyString = data.strip()
+
+        privatekey = keyfactory.parsePrivateKey(privateKeyString)
+        signature = privatekey.hashAndSign(raw)
+
+        return base64.b64encode(signature)
+
+
+def create_component(project_key, com_list, client):
+    # create component
+    com_template = {
+        "name": u"a",
+        "leadUserName": u"a",
+        "assigneeType": "COMPONENT_LEAD",
+        "isAssigneeTypeValid": False,
+        "project": project_key,
+    }
+
+    create_com_url = jira_server_url + '/rest/api/2/component'
+    for com in com_list:
+        # print "create component:" + com['name']
+        com_template['name'] = com['name']
+        if com['isAssigneeTypeValid'] and 'lead' in com:
+            com_template['leadUserName'] = com['lead']['key']
+            com_template['isAssigneeTypeValid'] = True
+        else:
+            com_template['leadUserName'] = u''
+            com_template['isAssigneeTypeValid'] = False
+        # com_templates.append(com_template)
+        content = json.dumps(com_template)
+        resp, content = client.request(method="POST", uri=create_com_url, headers=headers, body=content)
+        if resp['status'] != '201':
+            errmsg = 'Create comp ' + com_template['name'] + " Failed!!"
+            print errmsg
+    print "Finish creating component!!"
+
+
+def create_project(project_key, from_project_info, client):
+    # use jira workround api to create project with the same set of schemes
+    create_project_info = {
+        'key': project_key,
+        'lead': from_project_info['lead']['key'],
+        'name': project_key,
+    }
+
+    # create project
+    project_content = json.dumps(create_project_info)
+    create_project_url = jira_server_url + '/rest/project-templates/1.0/createshared/' + from_project_info['id']
+    resp, content = client.request(method="POST", uri=create_project_url, headers=headers, body=project_content)
+
+    if resp['status'] != '200':
+        errmsg = 'Create project ' + project_key + " Failed!!"
+        raise Exception(errmsg + "\n" + content)
+    print "Finish creating project scheme, now starting create comoponent...."
+
+    create_component(project_key, from_project_info['com_list'], client)
+    print "New project:" + project_key + " is ready!"
+
+
+def main(orig_args):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    consumer_key = 'OauthKey'
+    consumer_secret = 'dont_care'
+    consumer = oauth.Consumer(consumer_key, consumer_secret)
+
+    access_token = {'oauth_token': 'QHnpXBfHjXhYKfTMVDLMTyInQCefANgv', 'oauth_token_secret': 'k20W95a4ncIhSwBAcvlrXsIQn1IlZzcK'}
+    accessToken = oauth.Token(access_token['oauth_token'], access_token[
+                              'oauth_token_secret'])
+    client = oauth.Client(consumer, accessToken)
+    client.set_signature_method(SignatureMethod_RSA_SHA1())
+
+    from_project_key = orig_args[0]
+    from_project_url = jira_server_url + "/rest/api/2/project/" + from_project_key
+
+    project_keys = orig_args[1]
+    print project_keys
+    project_list = project_keys.split(',')
+
+    resp, content = client.request(from_project_url, "GET")
+    if resp['status'] != '200':
+        errmsg = 'Cloned project ' + from_project_key + " is not exist"
+        raise Exception(errmsg)
+
+    from_project_info = json.loads(content)
+
+    # get componment
+    get_com_url = jira_server_url + '/rest/api/2/project/' + from_project_key + '/components'
+    resp, content = client.request(get_com_url, "GET")
+    if resp['status'] != '200':
+        errmsg = 'Get components from the project ' + from_project_key + " Failed!!"
+        raise Exception(errmsg + "\n" + content)
+
+    coms = json.loads(content)
+    from_project_info['com_list'] = coms
+
+    for project_key in project_list:
+        create_project(project_key, from_project_info, client)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
